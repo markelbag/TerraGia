5d19d8ded9f410bfaa26a4d0eaa3573b39d75785
Mark <marcuselbag@icloud.com>
2021-11-05 09:32:02 -0400

    Added clickHandlers to all food and drinks in the menu. Fixed layout issues.

--- src/components/menu/menuDrinks.js
+++ src/components/menu/menuDrinks.js
@@ -1,20 +1,27 @@
 import * as React from "react"
 import { Drinks } from "../../../data"
 
-const MenuDrinks = (props) => (
+const MenuDrinks = (props) => {
+  const clickHander = () => {
+    console.log('Clicked')
+  }
+
+return (
     <div>
         {Drinks.map((item, itemIdx) => (
             <div key={item.id} className={itemIdx % 2 === 0 }>
               <div >
              <h1 className="text-4xl font-extralight pt-6">{item.type}</h1>
+             <button onClick={clickHander}>
              <h2 className="flex w-56 justify-between font-extralight text-xl">
                {item.item}
                <div>{item.name}</div><div>{item.price} </div></h2>
-             <p className="tgGreen text-sm w-48"><i>{item.ingrediants}{item.blend}</i></p>
+             <p className="flex tgGreen text-sm w-48 text-left"><i>{item.ingrediants}{item.blend}</i></p>
+              </button>
               </div>
             </div>
           ))}
     </div>
-)
+)}
 
 export default MenuDrinks;
--- src/components/menu/menuFood.js
+++ src/components/menu/menuFood.js
@@ -1,21 +1,27 @@
 import * as React from "react"
 import { Food } from "../../../data"
 
-const MenuFood = (props) => (
+const MenuFood = (props) => {
+    const clickHander = () => {
+      console.log('Clicked')
+    }
+    return (
     <div>
         {Food.map((item, itemIdx) => (
             <div key={item.id} className={itemIdx % 2 === 0 }>
               <div >
              <h1 className="text-4xl font-extralight pt-6">{item.type}</h1>
-             <div className="w-60">{item.item}{item.choice}</div>
-             <h2 className="flex w-56 justify-between font-extralight text-xl">
+             <div className="flex w-60 text-left">{item.item}{item.choice}</div>
+             <button onClick={clickHander}>
+             <h2 className="flex w-56 text-left justify-between font-extralight text-xl">
                <div>{item.name}</div><div>{item.price} </div></h2>
-             <p className="tgGreen text-sm w-48"><i>{item.ingrediants}{item.blend}
+             <p className="flex tgGreen text-sm w-48 text-left"><i>{item.ingrediants}{item.blend}
              <div>{item.choices}</div></i></p>
+             </button>
               </div>
             </div>
           ))}
     </div>
-)
+    )}
 
 export default MenuFood;
