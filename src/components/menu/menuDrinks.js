05ead7adc4072735973b971403588d1620d84ffd
Mark <marcuselbag@icloud.com>
2021-11-08 19:03:03 -0500

    Added modal(blank) to menu items

--- src/components/menu/menuDrinks.js
+++ src/components/menu/menuDrinks.js
@@ -1,13 +1,10 @@
 import * as React from "react"
+import {useState} from 'react'
 import { Drinks } from "../../../data"
 import AddToCartModal from "../addToCartModal/addToCartModal"
 
 const MenuDrinks = (props) => {
-  const clickHander = () => {
-    console.log('Clicked')
-    return(
-    <AddToCartModal />
-    )}
+  const [show, setShow] = useState(false)
 
 return (
     <div>
@@ -15,12 +12,13 @@ return (
             <div key={item.id} className={itemIdx % 2 === 0 }>
               <div >
              <h1 className="text-4xl font-extralight pt-6">{item.type}</h1>
-             <button onClick={clickHander}>
+             <button onClick={() => setShow(true)}>
              <h2 className="flex w-56 justify-between font-extralight text-xl">
                {item.item}
                <div>{item.name}</div><div>{item.price} </div></h2>
              <p className="flex tgGreen text-sm w-48 text-left"><i>{item.ingrediants}{item.blend}</i></p>
               </button>
+              <AddToCartModal show={show} setShow={setShow}/>
               </div>
             </div>
           ))}
